dcfaa1d3c37f514a21ae80c62cb2cbc233fbeaa9
Nik Sokolov <59799719+AnotherFinch@users.noreply.github.com>
2021-02-24 23:59:22 +0300

    Create MathOperation.java

---
+++ src/main/java/MathOperation.java
@@ -0,0 +1,9 @@
+public interface MathOperation {
+
+  public double sum(double s, double f);
+  public double minus (double s, double f);
+  public double separate (double s, double f);
+  public double multiplication (double s, double f);
+  public double findNum();
+  public String getOperation();
+}
